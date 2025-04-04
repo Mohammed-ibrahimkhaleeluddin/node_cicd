47a242b7b2dbd79ca4d6684b68eabeeed822e3aa
Mohammed-ibrahimkhaleeluddin <130655567+Mohammed-ibrahimkhaleeluddin@users.noreply.github.com>
2025-04-04 17:06:31 +0530

    Update cms.js

--- cms.js
+++ cms.js
@@ -1,11 +1,11 @@
 var express = require('express')
 var app = express()
 
-app.set('port', (process.env.PORT || 7000))
+app.set('port', (process.env.PORT || 7001))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello Nagendra it is v1.5')
+  response.send('Hello Nagendra it is v1.7')
 });
 
 app.listen(app.get('port'), function() {
